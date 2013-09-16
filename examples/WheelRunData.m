1636dbd991d98e8c7a6e10295d94a99e9c9f3eed
Barry Wark <barrywark@gmail.com>
2013-09-16 17:11:53 -0400

    Update example for 2.0 API

--- examples/WheelRunData.m
+++ examples/WheelRunData.m
@@ -2,27 +2,16 @@
 % This file loads all spikes from segments in which the mouse was running
 % CW in the wheel
 
+import ovation.*
+
 %% Connect to database
-ctx = NewDataContext('objectivity.int.janelia.org::/misc/ovation/pastalkova/pastalkova.connection');
+ctx = NewDataContext();
 
-%% Find the EpochGroup
+%% Retrieve an EpochGroup from the database, replacint EPOCH_GROUP_URI with the URI of the desired Epoch
+epochGroup = ctx.getObjectWithURI(EPOCH_GROUP_URI);
 
-% Get projects with name "Rat electrophysiology"
-projects = ctx.getProjects('Rat experiments');
-project = projects(1);
-
-% Get all experiments from this project. You can retrieve experiments by
-% date (see the documentation for project.getExperiments)
-experiments = project.getExperiments();
-experiment = experiments(1);
-
-% Get all EpochGroups with the label "1-3 arm Alternation+wheel"
-epochGroups = experiment.getEpochGroups('1-3 arm Alternation+wheel');
-epochGroup = epochGroups(1);
-
-% Get a list of Epochs from this EpochGroup
-epochs = epochGroup.getEpochs();
-
+%% Collect Epochs
+epochs = asarray(epochGroup.getEpochs());
 
 %% Collect wheel run CW spikes from each Epoch
 % We iterate over each epoch, pulling all timeline annotations from the
@@ -37,28 +26,40 @@ for i = 1:length(epochs)
     disp(['Epoch ' num2str(i) '...']);
     epoch = epochs(i);
     
+    deviceParameters = map2struct(epoch.getDeviceParameters());
+    lfpSampleRateHz = deviceParameters.lfpSampleRate;
+    
     % Get the spike times (as indexes @ 1250Hz)
-    spikeDerivedResponses = epoch.getDerivedResponses('spike-index-lfp');
-    % If there was more than one spike-index-lfp derived response (i.e.
-    % from more than one user, you would have to choose which one to use,
-    % or use yours with getMyDerivedResponse('spike-index-lfp')
-    assert(length(spikeDerivedResponses) == 1, 'For this demo, we expect only one. If there are more, you should choose which one to use');
-    spikeDerivedResponse = spikeDerivedResponses(1);
-    spikeIndexes = spikeDerivedResponse.getFloatingPointData();
-    
-    
-    % Collect wheel run (CW) spike indexes:
-    wheelRuns = {};
-    
-    wheelRunAnnotations = epoch.getTimelineAnnotations('wheel-runs-cw');
-    for j = 1:length(wheelRunAnnotations)
-        wheelRunAnnotation = wheelRunAnnotations(j);
-        startIndex = wheelRunAnnotation.getOwnerProperty('lfpStartIndex');
-        endIndex = wheelRunAnnotation.getOwnerProperty('lfpEndIndex');
-        
-        wheelRuns{j} = spikeIndexes(startIndex <= spikeIndexes & spikeIndexes <= endIndex); %#ok<SAGROW>
+    analysisRecords = asarray(epoch.getAnalysisRecords(ctx.getAuthenticatedUser));
+    spikeIndexRecord = [];
+    for j = 1:length(analysisRecords)
+        if(analysisRecords(j).getName().equals('Spikes'))
+            spikeIndexRecord = analysisRecords(j);
+            break;
+        end
     end
     
-    spikes{i} = wheelRuns; %#ok<SAGROW>
+    if(~isempty(spikeIndexRecord));
+        
+        spikeIndexes = nm2data(spikeIndexRecord.getOutputs().get('spike-index-lfp'));
+        
+        
+        % Collect wheel run (CW) spike indexes:
+        wheelRuns = {};
+        
+        timelineAnnotations = asarray(epoch.getUserTimelineAnnotations(ctx.getAuthenticatedUser()));
+        for j = 1:length(timelineAnnotations)
+            if(timelineAnnotations(j).getName().startsWith('wheel-runs-cw'))
+                wheelRunAnnotation = timelineAnnotations(j);
+                range = wheelRunAnnotation.getTimeRange();
+                startIndex = (range.lowerEndpoint().getMillis() - epoch.getStart.getMillis())/1000 * lfpSampleRateHz;
+                endIndex = (range.upperEndpoint().getMillis() - epoch.getStart.getMillis())/1000 * lfpSampleRateHz;
+                
+                wheelRuns{j} = spikeIndexes(startIndex <= spikeIndexes & spikeIndexes <= endIndex); %#ok<SAGROW>
+            end
+        end
+        
+        spikes{i} = wheelRuns; %#ok<SAGROW>
+    end
 end
 
